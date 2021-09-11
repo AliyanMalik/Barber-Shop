0cbf7f26576587996dff7a3561eedae58ddb22d0
Aliyan <Aliyan.malik.am@gmail.com>
2021-09-11 15:19:41 +0500

    Web Commit

--- src/components/Hero Section/Hero.js
+++ src/components/Hero Section/Hero.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import * as AiIcons from "react-icons/ai";
+import { IconContext } from "react-icons";
 import Menu from "../../assets/menu.svg";
 import Cart from "../../assets/cart.svg";
 import User from "../../assets/user.svg";
@@ -8,19 +8,81 @@ import User from "../../assets/user.svg";
 import "./Hero.css";
 
 const Hero = (props) => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+  const SidebarData = [
+    {
+      title: "Home",
+      cName: "nav-text",
+      id: 1,
+    },
+    {
+      title: "About Us",
+      cName: "nav-text",
+      id: 2,
+    },
+    {
+      title: "Shop",
+      cName: "nav-text",
+      id: 3,
+    },
+    {
+      title: "Portfolio",
+      cName: "nav-text",
+      id: 4,
+    },
+    {
+      title: "Contact Us",
+      cName: "nav-text",
+      id: 5,
+    },
+  ];
+  const scrollPage = (id) => {
+    if (id === 0) {
+      window.scrollTo(20, 20);
+    }
+    if (id === 1) {
+      window.scrollTo(0, 800);
+    }
+    if (id === 2) {
+      window.scrollTo(0, 2990);
+    }
+    if (id === 3) {
+      window.scrollTo(0, 4400);
+    }
+    if (id === 4) {
+      window.scrollTo(0, 5200);
+    }
+  };
+
   return (
     <section className="hero">
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <div to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </div>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <div id={item.id} onClick={() => scrollPage(index)}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
       <div className="nav">
-        <div className="menuWrapper navbar">
-          <Link to="#" className="menu-bars">
-            <img
-              src={Menu}
-              alt="Menu"
-              className="menu"
-              onClick={props.onClick}
-            />
-            <p className="text">Menu</p>
-          </Link>
+        <div to="#" className="menuWrapper" onClick={showSidebar}>
+          <img src={Menu} alt="Menu" className="menu" />
+          <p className="text">Menu</p>
         </div>
         <h1 className="heroTitle">3bershop</h1>
         <div className="cartSection">
--- src/components/Landing Page/LandingPage.js
+++ src/components/Landing Page/LandingPage.js
@@ -1,35 +1,22 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Hero from "../Hero Section/Hero";
 import Body from "../Body Section/Body";
-import "./LandingPage.css";
 import Products from "../Products/Products";
 import Discount from "../Discount/Discount";
 import Portfolio from "../Portfolio/Portfolio";
 import Footer from "../Footer/Footer";
-import Navbar from "../Navbar/Navbar";
+import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <React.Fragment>
-      <Router>
-        <Navbar />
-        <Hero />
-        <Body />
-        <Products />
-        <Discount />
-        <Portfolio />
-        <Footer />
-        <Switch>
-          <Route path="/" exact component={Hero} />
-          <Route path="/introduction" component={Body} />
-          <Route path="/products" component={Products} />
-          <Route path="/discount" component={Discount} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/Footer" component={Footer} />
-        </Switch>
-      </Router>
+      <Hero />
+      <Body />
+      <Products />
+      <Discount />
+      <Portfolio />
+      <Footer />
     </React.Fragment>
   );
 };
